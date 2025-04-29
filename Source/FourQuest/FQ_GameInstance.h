e27599f3ade7ffcff26cf52433bd2c0ada6d8203
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-04-29 10:37:13 +0900

    feat : FourQuest 프로젝트 기본 커밋

---
+++ Source/FourQuest/FQ_GameInstance.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "FQ_GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FOURQUEST_API UFQ_GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+
+
+private:
+
+};
