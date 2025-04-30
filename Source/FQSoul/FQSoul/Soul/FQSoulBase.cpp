22b20f4892a2481e40b54d4bbb8bdbbf7a0211f6
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-04-30 12:43:36 +0900

    feat. 영혼 클래스 생성 작업 진행중

---
+++ Source/FQSoul/FQSoul/Soul/FQSoulBase.cpp
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FQSoulBase.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework\CharacterMovementComponent.h"
+
+// Sets default values
+AFQSoulBase::AFQSoulBase()
+{
+	// Pawn
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	// Capsule
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.f);
+	//GetCapsuleComponent()->SetCollisionProfileName();
+
+	// Movement
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 500.f, 0.f);
+	GetCharacterMovement()->AirControl = 0.35f;
+	GetCharacterMovement()->MaxWalkSpeed = 500.f;
+	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+}
+
+// Called when the game starts or when spawned
+void AFQSoulBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFQSoulBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AFQSoulBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
