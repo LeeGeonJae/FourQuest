1c4de9ed686fb8c544cced965e22623eece64a3f
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2025-04-30 22:51:41 +0900

    feat : 소울 데이터 에셋 추가, 소울 기능 추가 - 소울 대쉬 추가

---
+++ Source/FQSoul/Data/FQSoulDataAsset.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FQSoulDataAsset.h"
+
+UFQSoulDataAsset::UFQSoulDataAsset()
+{
+	mWalkSpeed = 500.f;
+
+	mDashDuration = 0.2f;
+	mDashSpeed = 5000.f;
+}
