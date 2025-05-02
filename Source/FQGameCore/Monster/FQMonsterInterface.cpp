f4dc5f6b0d10e2d66f21ce4cad5d76dd2d4a008c
DESKTOP-H2TVKH1\win <firstep710@gmail.com>
2025-05-02 10:29:03 +0900

    Reapply "Feat : 몬스터 모듈 추가 Fix : SoulBase Interface 함수 구현부 수정"
    This reverts commit 152b1c581920570a17270036bbfb4a3bdc9c8ca9.

---
+++ Source/FQGameCore/Monster/FQMonsterInterface.cpp
@@ -0,0 +1,7 @@
+
+
+
+#include "FQMonsterInterface.h"
+
+
+// Add default functionality here for any IFQMonsterInterface functions that are not pure virtual.
---
+++ Source/FQMonster/FQMonster.Build.cs
@@ -0,0 +1,9 @@
+using UnrealBuildTool;
+
+public class FQMonster: ModuleRules
+{
+    public FQMonster(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PrivateDependencyModuleNames.AddRange(new string[] {"Core", "CoreUObject", "Engine", "FQGameCore" });
+    }
+}
