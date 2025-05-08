4cc2c7881a70d3bcef1607c6eb343abdc57a23b3
AriSpace00 <dhjj2110@gmail.com>
2025-05-08 11:05:41 +0900

    add. GameCore에 GameMode, PlayerController 추가

---
+++ Source/FourQuest/FourQuest/Framework/InGame/FQGameMode_InGame.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FQGameMode_InGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FOURQUEST_API AFQGameMode_InGame : public AGameModeBase
+{
+	GENERATED_BODY()
+};
---
+++ Source/FourQuest/FourQuest/Framework/InGame/FQPlayerController_InGame.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "FQPlayerController_InGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FOURQUEST_API AFQPlayerController_InGame : public APlayerController
+{
+	GENERATED_BODY()	
+};
