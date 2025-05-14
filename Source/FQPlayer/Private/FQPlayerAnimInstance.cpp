567faf435951f7d7df8e189ac605e7e1591098a3
AriSpace00 <dhjj2110@gmail.com>
2025-05-14 17:36:09 +0900

    feat. 플레이어 애니메이션 추가
    - 기울임 값 적용하여 애니메이션 블렌딩 조정되도록 구현

---
+++ Source/FQPlayer/Private/FQPlayerAnimInstance.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FQPlayerAnimInstance.h"
+#include "FQPlayer/Public/FQPlayerBase.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UFQPlayerAnimInstance::UFQPlayerAnimInstance()
+{
+	mMovingThreshould = 3.0f;
+}
+
+void UFQPlayerAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	mOwner = Cast<AFQPlayerBase>(GetOwningActor());
+	if (mOwner)
+	{
+		mMovement = Cast<UCharacterMovementComponent>(mOwner->GetCharacterMovement());
+	}
+}
+
+void UFQPlayerAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (!mMovement)
+	{
+		return;
+	}
+
+	mVelocity = mMovement->Velocity;
+	mGroundSpeed = mVelocity.Size2D();
+	mNormalizedSpeed = mGroundSpeed / mMovement->GetMaxSpeed();
+	mbIsIdle = mGroundSpeed < mMovingThreshould;
+}
---
+++ Source/FQPlayer/Public/FQPlayerAnimInstance.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "FQPlayerAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FQPLAYER_API UFQPlayerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	UFQPlayerAnimInstance();
+
+protected:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character")
+	TObjectPtr<class AFQPlayerBase> mOwner;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character")
+	TObjectPtr<class UCharacterMovementComponent> mMovement;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
+	FVector mVelocity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
+	float mGroundSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
+	float mNormalizedSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
+	uint8 mbIsIdle : 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
+	float mMovingThreshould;
+};
