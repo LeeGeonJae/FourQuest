6aaf98be5e21debca265b88b4b4dd6ee3e35bd5b
AriSpace00 <dhjj2110@gmail.com>
2025-06-09 11:15:08 +0900

    fix. 마법사 플레이어 X스킬 수정
    - 근처에 피격 가능 대상 없다면 전방으로 투사체 발사

--- Source/FQPlayer/Private/FQMagePlayer.cpp
+++ Source/FQPlayer/Private/FQMagePlayer.cpp
@@ -18,6 +18,7 @@ AFQMagePlayer::AFQMagePlayer()
 	mProjectileAttackVolume = CreateDefaultSubobject<UBoxComponent>(TEXT("ProjectileAttackVolume"));
 	mProjectileAttackVolume->SetupAttachment(RootComponent);
 
+	mLookAtDirection = FVector::ZeroVector;
 	mLookAtRotation = FRotator::ZeroRotator;
 }
 
@@ -232,7 +233,7 @@ void AFQMagePlayer::ProcessInputMovement()
 
 	// 마법진 위치 
 	FVector CurrentLocation = mExplosionCircle->GetActorLocation();
-	FVector DeltaMove = mMoveDir * mMageDataAsset->mExplosionCircleSpeed * GetWorld()->GetDeltaSeconds();
+	FVector DeltaMove = mMoveDirection * mMageDataAsset->mExplosionCircleSpeed * GetWorld()->GetDeltaSeconds();
 	FVector NewLocation = CurrentLocation + DeltaMove;
 
 	float Distance = FVector::Dist(GetActorLocation(), NewLocation);
@@ -635,12 +636,6 @@ void AFQMagePlayer::ProcessProjectileAttack()
 		// 투사체 초기화 및 발사
 		Projectile->SetLifeSpan(mMageDataAsset->mProjectileDuration);
 		Projectile->SetCount(mMageDataAsset->mProjectileCount);
-
-		if (mLookAtDirection.IsZero())
-		{
-			mLookAtDirection = GetActorForwardVector();
-		}
-
 		Projectile->LaunchProjectile(mLookAtDirection, mMageDataAsset->mProjectileSpeed);
 
 		UE_LOG(LogTemp, Log, TEXT("[ProcessProjectileAttack] 생성 성공"));
@@ -653,6 +648,17 @@ void AFQMagePlayer::ProcessProjectileAttack()
 
 void AFQMagePlayer::CheckProjectileAttackVolume()
 {
+	if (mMoveDirection.IsZero())
+	{
+		mLookAtDirection = GetActorForwardVector();
+	}
+	else
+	{
+		mLookAtDirection = mMoveDirection;
+	}
+	
+	mLookAtRotation = mLookAtDirection.Rotation();
+
 	float Distance = TNumericLimits<float>::Max();
 	TArray<AActor*> OverlappedActors;
 	mProjectileAttackVolume->GetOverlappingActors(OverlappedActors);
